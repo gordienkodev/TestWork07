91a97b1edd41f52fe65fa9c44d6051a9a9d587d6
Pavel Gordienko <guzmo@yandex.ru>
2025-03-30 18:25:31 +0300

    feat: add city weather

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import { Search } from '../components/Search'
+import { CityWeather } from '../components/CityWeather/CityWeather'
+import { Search } from '../components/Search/Search'
 import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main
-        className={`${styles.custom_gradient} d-flex justify-content-center align-items-center vh-100 w-100`}
+        className={`${styles.custom_gradient} d-flex flex-column gap-3 justify-content-center align-items-center vh-100 w-100`}
       >
         <Search />
+        <CityWeather />
       </main>
     </div>
   )
