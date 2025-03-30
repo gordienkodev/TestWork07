3aa69ef9039ca8fcfa25000a5fab13cd7a4f8346
Pavel Gordienko <guzmo@yandex.ru>
2025-03-30 23:32:26 +0300

    feat: add hook store for current city

--- src/components/CityWeather/CityWeather.tsx
+++ src/components/CityWeather/CityWeather.tsx
@@ -1,15 +1,18 @@
-import { mockForecast, mockForecastList } from './data'
+'use client'
+import { useWeather } from '../../hooks/useWeather'
 import styles from './CityWeather.module.scss'
 
 export const CityWeather = () => {
+  const { weather, loading, error } = useWeather()
   return (
     <div
       className={`${styles.custom_styles} w-100 p-4 d-flex flex-column text-center align-items-center justify-content-center px-md-5 rounded shadow-lg`}
     >
-      <h2>
-        {mockForecast?.name}: {mockForecast?.country}
-      </h2>
-      <h3>{Math.round(mockForecastList?.list[0]?.main?.temp)}°C</h3>
+      <h2>{weather?.city?.name}</h2>
+
+      {loading && <p>loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {weather && <h3>{Math.round(weather?.list[0]?.main?.temp)}°C</h3>}
     </div>
   )
 }
