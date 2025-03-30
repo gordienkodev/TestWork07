e511e7aed3960ed5a413426e9d18b4d7899e481e
Pavel Gordienko <guzmo@yandex.ru>
2025-03-30 22:55:18 +0300

    fix: config remove webpack

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from 'next'
-import path from 'path'
 
 const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `$var: red;`,
-  },
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@/': path.resolve(process.cwd(), 'src'),
-    }
-    return config
-  },
+  }
 }
 
 export default nextConfig
