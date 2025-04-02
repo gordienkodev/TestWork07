1769e8d9a318ce4dccf9c09e237bfba9c093f89c
Pavel Gordienko <guzmo@yandex.ru>
2025-04-02 23:42:04 +0300

    fix: clear unusable interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,111 +1,100 @@
 export interface ICity {
-  name: string
-  country: string
-  lat: number
-  lon: number
+    name: string
+    country: string
+    lat: number
+    lon: number
 }
 
 export interface ICityCardProps {
-  city: ICity
+    city: ICity
 }
 
 export type TCurrentSearchState = {
-  currentSearch: ICity | null
-  setCurrentSearch: (city: ICity) => void
+    currentSearch: ICity | null
+    setCurrentSearch: (city: ICity) => void
 }
 
 export type TForecastType = {
-  name: string
-  country: string
-  list: {
-    dt: number
-    main: {
-      feels_like: number
-      humidity: number
-      pressure: number
-      temp: number
-      temp_max: number
-      temp_min: number
-    }
-    weather: {
-      main: string
-      icon: string
-      description: string
+    name: string
+    country: string
+    list: {
+        dt: number
+        main: {
+            feels_like: number
+            humidity: number
+            pressure: number
+            temp: number
+            temp_max: number
+            temp_min: number
+        }
+        weather: {
+            main: string
+            icon: string
+            description: string
+        }[]
+        wind: {
+            speed: number
+            gust: number
+            deg: number
+        }
+        clouds: {
+            all: number
+        }
+        pop: number
+        visibility: number
     }[]
-    wind: {
-      speed: number
-      gust: number
-      deg: number
-    }
-    clouds: {
-      all: number
-    }
-    pop: number
-    visibility: number
-  }[]
 }
 
 export interface ICityWeatherProps {
-  forecast: TForecastType
-  selectedCity: ICity
+    forecast: TForecastType
+    selectedCity: ICity
 }
 
 export interface IFavoriteButtonProps {
-  selectedCity: ICity
+    selectedCity: ICity
 }
 
 export interface IFavoritesCityProps {
-  favoriteCities: ICity[]
+    favoriteCities: ICity[]
 }
 
 export interface IForecastOverviewProps {
-  forecast: TForecastType
-  currentSearch: ICity
+    forecast: TForecastType
+    currentSearch: ICity
 }
 
 export type TDailyForecast = {
-  date: string
-  temp: number
-  feels_like: number
-  humidity: number
-  wind: number
-  description: string
-  icon: string
+    date: string
+    temp: number
+    feels_like: number
+    humidity: number
+    wind: number
+    description: string
+    icon: string
 }
 
 export interface ISearchProps {
-  onSearchSubmit: (selectedCity: ICity) => void
-}
-
-export interface IWeatherData {
-  city: {
-    name: string
-  }
-  list: {
-    main: {
-      temp: number
-    }
-  }[]
+    onSearchSubmit: (selectedCity: ICity) => void
 }
 
 export type TSearchHistoryState = {
-  history: ICity[]
-  addSearch: (city: ICity) => void
-  getCityForecast: (name: string, country: string) => ICity | undefined
+    history: ICity[]
+    addSearch: (city: ICity) => void
+    getCityForecast: (name: string, country: string) => ICity | undefined
 }
 
 export interface IWeatherStore {
-  city: ICity
-  setCity: (city: ICity) => void
+    city: ICity
+    setCity: (city: ICity) => void
 }
 
 export interface IFavoritesStore {
-  favorites: ICity[]
-  addToFavorites: (city: ICity) => void
-  removeFromFavorites: (city: ICity) => void
+    favorites: ICity[]
+    addToFavorites: (city: ICity) => void
+    removeFromFavorites: (city: ICity) => void
 }
 
 export interface IApiResponse {
-  city: ICity
-  list: TForecastType['list']
+    city: ICity
+    list: TForecastType['list']
 }
